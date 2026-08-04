0de20991b838bb93bcf5baee5cb605bd2289f08e
Graeme Gets <graeme@athousandprojects.com>
2026-08-04 10:23:16 +0100

    cbuffer: Refactor to circular_buffer_t and add new operations

--- kernel/include/kernel/cbuffer.h
+++ kernel/include/kernel/cbuffer.h
@@ -1,21 +1,26 @@
 #ifndef _CBUFFER_H_
-#define CBUFFER_H_
+#define _CBUFFER_H_
 #include <stdint.h>
+
 #define BUFFER_SIZE 64
+
+#define CBUFFER_INIT_NO  0
+#define CBUFFER_INIT_YES 1
 
 typedef struct
 {
-uint8_t buffer[BUFFER_SIZE];
-volatile uint8_t write_index;
-volatile uint8_t read_index;
-}circular_buffer;
+    uint8_t buffer[BUFFER_SIZE];
+    volatile uint8_t write_index;
+    volatile uint8_t read_index;
+    char init_status;
+} circular_buffer_t;
 
-void cbuffer_initialize(circular_buffer *cbuffer);
-void cbuffer_put(circular_buffer *cbuffer, uint8_t data);
-uint8_t cbuffer_get(circular_buffer *cbuffer);
-uint8_t cbuffer_data_available(circular_buffer *cbuffer);
-uint8_t cbuffer_is_empty(circular_buffer *cbuffer);
-uint8_t cbuffer_is_full(circular_buffer *cbuffer);
-
-
+void cbuffer_initialize(circular_buffer_t *cbuffer);
+void cbuffer_put(circular_buffer_t *cbuffer, uint8_t data);
+uint8_t cbuffer_get(circular_buffer_t *cbuffer);
+uint8_t cbuffer_data_available(circular_buffer_t *cbuffer);
+uint8_t cbuffer_is_empty(circular_buffer_t *cbuffer);
+uint8_t cbuffer_is_full(circular_buffer_t *cbuffer);
+uint8_t cbuffer_peek(circular_buffer_t *cbuffer);
+uint8_t cbuffer_find(circular_buffer_t *cbuffer,uint8_t chr);
 #endif
--- kernel/kernel/cbuffer.c
+++ kernel/kernel/cbuffer.c
@@ -1,26 +1,29 @@
 
 #include "kernel/cbuffer.h" 
 
-void cbuffer_initialize(circular_buffer *cbuffer)
+void cbuffer_initialize(circular_buffer_t *cbuffer)
 {
-    cbuffer->write_index = 0;
-    cbuffer->read_index = 0;
+    if (cbuffer->init_status == CBUFFER_INIT_YES)
+    {
+        cbuffer->write_index = 0;
+        cbuffer->read_index = 0;
+    }
 }
 
 // Check if buffer is full
-uint8_t cbuffer_is_full(circular_buffer *cbuffer)
+uint8_t cbuffer_is_full(circular_buffer_t *cbuffer)
 {
     return ((cbuffer->write_index + 1) % BUFFER_SIZE) == cbuffer->read_index;
 }
 
 // Check if buffer is empty
-uint8_t cbuffer_is_empty(circular_buffer *cbuffer)
+uint8_t cbuffer_is_empty(circular_buffer_t *cbuffer)
 {
     return cbuffer->write_index == cbuffer->read_index;
 }
 
 // Put data
-void cbuffer_put(circular_buffer *cbuffer, uint8_t data)
+void cbuffer_put(circular_buffer_t *cbuffer, uint8_t data)
 {
     if (!cbuffer_is_full(cbuffer))
     {
@@ -30,7 +33,7 @@ void cbuffer_put(circular_buffer *cbuffer, uint8_t data)
 }
 
 // Get data
-uint8_t cbuffer_get(circular_buffer *cbuffer)
+uint8_t cbuffer_get(circular_buffer_t *cbuffer)
 {
     if (cbuffer_is_empty(cbuffer))
     {
@@ -42,8 +45,55 @@ uint8_t cbuffer_get(circular_buffer *cbuffer)
     return data;
 }
 
+uint8_t cbuffer_find(circular_buffer_t *cbuffer,uint8_t chr)
+{
+    uint8_t ptr_read = cbuffer->read_index;
+    
+    while (ptr_read != cbuffer->write_index)
+    {
+        uint8_t data = cbuffer->buffer[ptr_read];
+        if (data == chr)
+            return ptr_read;
+        ptr_read = (ptr_read+1) % BUFFER_SIZE;
+    }
+    return 0;
+     
+}
+// peek last character
+uint8_t cbuffer_peek(circular_buffer_t *cbuffer)
+{
+    if (cbuffer_is_empty(cbuffer))
+    {
+        return 0;  // or handle error
+    }
+
+    uint8_t data = cbuffer->buffer[cbuffer->read_index];
+    
+    return data;
+}
+
+uint8_t cbuffer_first_to_CR(circular_buffer_t *cbuffer,char *data,uint8_t len)
+{
+    if (!cbuffer_data_available(cbuffer))
+    {
+        return 0;
+    }
+    uint8_t chr = 0x0;
+    while (chr != 0x0D)
+    {
+        chr = cbuffer_get(cbuffer);
+        *data = chr;
+        data++;
+    }
+    return 0;
+}
+
 // Data available
-uint8_t cbuffer_data_available(circular_buffer *cbuffer)
+uint8_t cbuffer_data_available(circular_buffer_t *cbuffer)
 {
     return (cbuffer->write_index + BUFFER_SIZE - cbuffer->read_index) % BUFFER_SIZE;
 }
+
+
+//* Build a listerer for the keyboard - reg console as only listener. 
+//** build a 'pass back string up to CR'
