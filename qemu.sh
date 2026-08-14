ec736754194b64c6cb0d72747bfc054ff8423664
Graeme Gets <graeme@athousandprojects.com>
2026-08-14 11:57:00 +0100

    build: Update QEMU script for 64-bit emulation and debugging

--- qemu.sh
+++ qemu.sh
@@ -4,12 +4,15 @@ set -e
 . ./iso.sh
 
 echo "About to lauch QEMU"
-while getopts "d" opts; do
+while getopts "d","n" opts; do
     case $opts in
     d) 
         echo "Set up for GDB"
         qemu-system-$(./target-triplet-to-arch.sh $HOST) -s -S -cdrom zenos.iso
         exit 1
+        ;;
+    n)  echo "Set up with No Reboot"
+        qemu-system-x86_64 -d int -no-reboot -no-shutdown -cpu qemu64 -cdrom zenos.iso
         ;;
     ?)
         echo "No Selection"
@@ -20,4 +23,5 @@ while getopts "d" opts; do
 done
 echo "Running Default"
 #qemu-system-$(./target-triplet-to-arch.sh $HOST) -s -S -cdrom zenos.iso
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom zenos.iso
+#qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom zenos.iso
+qemu-system-x86_64 -cpu qemu64 -cdrom zenos.iso
